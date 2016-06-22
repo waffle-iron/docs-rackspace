414cd3eae963d754be1c06cfab6d9de75cfdb822
Brian Moss <kallimachos@gmail.com>
2016-06-22 10:46:29 +1000

    Tweak deploy.sh (#144)
    * Tweak deploy.sh
    
    * Add message if not deploying to gh-pages

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -9,7 +9,7 @@ if [ "$TRAVIS_REPO_SLUG" == "rackerlabs/docs-rackspace" ] && [ "$TRAVIS_PULL_REQ
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/rackerlabs/docs-rackspace gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://$GH_TOKEN@github.com/rackerlabs/docs-rackspace gh-pages > /dev/null
 
   cd gh-pages
   find * -not -name ".*" -delete
@@ -18,7 +18,15 @@ if [ "$TRAVIS_REPO_SLUG" == "rackerlabs/docs-rackspace" ] && [ "$TRAVIS_PULL_REQ
   git commit -m "Latest doc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
 
-  tput setaf 2; echo "Docs published to http://rackerlabs.github.io/docs-rackspace/."
-  tput sgr0
+  if test `tput -T $TERM colors` -lt 256; then
+    echo "Docs published to http://rackerlabs.github.io/docs-rackspace/."
+  else
+    tput -T $TERM setaf 2
+    echo "Docs published to http://rackerlabs.github.io/docs-rackspace/."
+    tput -T $TERM sgr0
+  fi
+
+else
+    echo -e "Not a merge to master. Not deploying to gh-pages."
 
 fi
