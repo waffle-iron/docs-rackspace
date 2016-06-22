993719a79cb64c904804d5108427a7e27550f9da
Brian Moss <kallimachos@gmail.com>
2016-06-22 11:09:42 +1000

    Fix paths in deploy.sh (#145)
    * Fix paths in deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_REPO_SLUG" == "rackerlabs/docs-rackspace" ] && [ "$TRAVIS_PULL_REQ
 
   echo -e "Publishing gh-pages...\n"
 
-  cp -R _build/docs/html $HOME/html
+  cp -R doc/_build/html $HOME/html
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
@@ -14,15 +14,15 @@ if [ "$TRAVIS_REPO_SLUG" == "rackerlabs/docs-rackspace" ] && [ "$TRAVIS_PULL_REQ
   cd gh-pages
   find * -not -name ".*" -delete
   cp -Rf $HOME/html ./
-  git add -f .
+  git add .
   git commit -m "Latest doc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
 
   if test `tput -T $TERM colors` -lt 256; then
-    echo "Docs published to http://rackerlabs.github.io/docs-rackspace/."
+    echo "Docs published to http://rackerlabs.github.io/docs-rackspace/"
   else
     tput -T $TERM setaf 2
-    echo "Docs published to http://rackerlabs.github.io/docs-rackspace/."
+    echo "Docs published to http://rackerlabs.github.io/docs-rackspace/"
     tput -T $TERM sgr0
   fi
 
