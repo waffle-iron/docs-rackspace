456fcca9603d6f79deed6391fd3056cb30d04813
Brian Moss <kallimachos@gmail.com>
2016-10-18 15:30:26 +1000

    Add guilabel and menuselection role styling (#218)
    guilabel and menuselection are standard Sphinx
    roles but they do not have a default styling.
    
    Set the styling for both roles to bold.
    
    rackerlabs/docs-workstream#89

--- doc/conf.py
+++ doc/conf.py
@@ -165,6 +165,7 @@ html_static_path = ['_static']
 html_context = {
     'css_files': [
         '_static/theme_overrides.css',  # overrides wide tables in RTD theme
+        '_static/custom.css',  # custom CSS styling
         ],
     }
 
